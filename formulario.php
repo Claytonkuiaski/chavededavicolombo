48eef38bf993c26a240e3dba36f7c4b39081688a
claytonKuiaski <claytonKuiaski@hotmail.com>
2017-05-13 22:30:41 -0300

    finalizado

--- formulario.php
+++ formulario.php
@@ -1,28 +1,23 @@
 <?php
 
-//recuperar dados do formulario
 $GetPost = filter_input_array(INPUT_POST,FILTER_DEFAULT);
 
-//Variáveis locais
 $Erro     = true;
 $Nome     = $GetPost['nome'];
 $Email    = $GetPost['email'];
 $Fone    = $GetPost['fone'];
 $Mensagem = $GetPost['mensagem'];
 
-//Incluir a classe PHPMailer
 include_once 'PHPMailer/class.smtp.php';
 include_once 'PHPMailer/class.phpmailer.php';
 
-//Enviando o e-mail utilizando a classe PHPMailer
 $Mailer = new PHPMailer;
 $Mailer->CharSet = "utf-8";
-//$Mailer->SMTPDebug = 3;
 $Mailer->IsSMTP();
 $Mailer->Host = 'a2plcpnl0501.prod.iad2.secureserver.net';
 $Mailer->SMTPAuth = true;
 $Mailer->Username = "contato@chavededavicolombo.com.br";
-$Mailer->Password = "keycolombo";
+$Mailer->Password = "**********";
 $Mailer->SMTPSecure = "ssl";
 $Mailer->Port = 465;
 $Mailer->FronName = "{$Nome}";
@@ -32,10 +27,8 @@ $Mailer->IsHTML(true);
 $Mailer->Subject = "Cadastro - Página de contato";
 $Mailer->Body = "{$Mensagem}<br><br>Nome: {$Nome}<br>E-mail: {$Email}<br>Telefone: {$Fone}";
 
-//Verificação
 $enviado = $Mailer->Send();
 
-//Exibe uma mensagem de resultado
 if ($enviado){
 	header("Location: dadosenviados.html");
 }   else {
--- news.php
+++ news.php
@@ -1,27 +1,22 @@
 <?php
 
-//recuperar dados do formulario
 $GetPost = filter_input_array(INPUT_POST,FILTER_DEFAULT);
 
-//Variáveis locais
 $Erro     = true;
 //$Nome     = $GetPost['nome'];
 $Email    = $GetPost['email'];
 //$Mensagem = $GetPost['mensagem'];
 
-//Incluir a classe PHPMailer
 include_once 'PHPMailer/class.smtp.php';
 include_once 'PHPMailer/class.phpmailer.php';
 
-//Enviando o e-mail utilizando a classe PHPMailer
 $Mailer = new PHPMailer;
 $Mailer->CharSet = "utf-8";
-//$Mailer->SMTPDebug = 3;
 $Mailer->IsSMTP();
 $Mailer->Host = 'a2plcpnl0501.prod.iad2.secureserver.net';
 $Mailer->SMTPAuth = true;
 $Mailer->Username = "contato@chavededavicolombo.com.br";
-$Mailer->Password = "keycolombo";
+$Mailer->Password = "**********";
 $Mailer->SMTPSecure = "ssl";
 $Mailer->Port = 465;
 $Mailer->FronName = "";
@@ -31,10 +26,8 @@ $Mailer->IsHTML(true);
 $Mailer->Subject = "Cadastro de e-mail para notícias";
 $Mailer->Body = "E-mail: {$Email}";
 
-//Verificação
 $enviado = $Mailer->Send();
 
-//Exibe uma mensagem de resultado
 if ($enviado){
 	header("Location: dadosenviados.html");
 }   else {
