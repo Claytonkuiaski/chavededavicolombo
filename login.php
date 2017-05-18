38e943c6edf0630fdaa790e50408bc235fa047ac
claytonKuiaski <claytonKuiaski@hotmail.com>
2017-05-17 23:09:09 -0300

    final

--- login.php
+++ login.php
@@ -4,8 +4,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
-	<title>Chave de Davi Colombo</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Ministério de Louvor, adoração, ensino e missões. Igreja com a visão dos 5 ministérios. Area restrita a lideres">
+  <meta name="robots" content="index, follow, archive" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="HqPJbm88E-U8DvsIZ-omDXLvpH3NOQxVUNg_By33jN4" />
+  <title>Comunidade Cristã Chave de Davi Colombo - Login</title>
+  <link rel="shortcut icon" type="image/x-icon" href="images/favi.ico">
 	<link rel="stylesheet" href="css/geral.css">
 	<link rel="stylesheet" href="css/plugins.css">
   <link rel="stylesheet" href="css/fonts.css">
@@ -16,7 +21,7 @@
 	<div class="area-login">
 		<div class="container">
 			<div class="login">
-				<img src="images/chave.png" alt="">
+				<img src="images/chave.png" alt="logotipo">
 				<h2>ÁREA RESTRITA</h2>
 				<form action="valida.php" method="post">
 					<input type="text" name="login" placeholder="login"><br><br>
